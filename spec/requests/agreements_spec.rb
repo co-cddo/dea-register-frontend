ae61ac254909e10be8919640d739469ee76b11a4
Rob Nichols <rob@nicholshayes.co.uk>
2023-06-06 13:54:31 +0100

    Add agreements controller with show and index views
    agreements#index set as root path

---
+++ spec/requests/agreements_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Agreements", type: :request do
+  let(:fields) { { foo: :bar } }
+  let!(:agreement) { create :agreement, fields: fields }
+
+  describe "GET root (index)" do
+    it "returns http success" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "displays link to agreement" do
+      get root_path
+      expect(response.body).to include(agreement_path(agreement))
+    end
+  end
+
+  describe "GET /agreements/:id show" do
+    it "returns http success" do
+      get agreement_path(agreement)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "displays agreement fields" do
+      get agreement_path(agreement)
+      expect(response.body).to include('Foo')
+      expect(response.body).to include('bar')
+    end
+  end
+end
