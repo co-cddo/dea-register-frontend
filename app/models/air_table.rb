0ad9c6ca76762ba2b586d86c1c79da136a92d67d
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2023-06-15 10:04:17 +0100

    Add method to search Airtable table data.
    Also refactor AirTable to simplify code and reuse elements used by
    both .populate and .search

--- app/models/air_table.rb
+++ app/models/air_table.rb
@@ -1,13 +1,54 @@
+# Classes that inherit from this one store data for each of the Airtable tables
+# For example, if you want to store the data for an AirTable table "Foo bar" you can:
+# ```
+#   class FooBar < AirTable
+#     self.air_table_name = 'Foo bar'
+#   end
+# ```
+#
+# With that in place, `FooBar.populate` will pull each of the records from AirTable and
+# store them in the air_tables table with the type 'FooBar'.
 class AirTable < ApplicationRecord
-  extend AirTablePopulate
-
   class << self
     attr_accessor :air_table_name
 
     def populate
       raise "Cannot run on root class - AirTable" if self == AirTable
 
-      populate_from_air_table(self, air_table_name)
+      populate_from_air_table
+    end
+
+    def search(text)
+      query = { "filterByFormula" => %[SEARCH("#{text}",{Name})] }
+      data = AirTableApi.data_for(air_table_path, query:)
+      return [] if data[:records].empty?
+
+      ids = data[:records].pluck(:id)
+      where(record_id: ids)
+    end
+
+  private
+
+    def populate_from_air_table
+      data = {}
+      # API returns 100 records at a time.
+      # If there are more records, API returns an offset key that needs to be
+      # passed into the next query
+      while data.empty? || data[:offset].present?
+        query = data[:offset].present? ? { offset: data[:offset] } : {}
+        data = AirTableApi.data_for(air_table_path, query:)
+        data[:records].each do |record|
+          instance = find_or_initialize_by(record_id: record[:id])
+          instance.fields = record[:fields]
+          instance.name = record.dig(:fields, :name) || record.dig(:fields, :Name)
+          instance.save!
+        end
+      end
+    end
+
+    def air_table_path
+      table_id = AirTableTable.id_for_name(air_table_name)
+      "#{AirTableBase.base_id}/#{table_id}"
     end
   end
 end
--- spec/factories/air_tables.rb
+++ spec/factories/air_tables.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :air_table do
     name { Faker::Company.industry }
     record_id { SecureRandom.uuid }
-    type { %w[People Party Agreement Power].sample }
+    type { %w[Agreement].sample }
   end
 end
