1ab750f150ea124c36462c1188f8c24ddaddcfe5
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2023-08-14 15:50:26 +0100

    Ensure stale agreement associations are removed during population

--- app/models/agreement_control_person.rb
+++ app/models/agreement_control_person.rb
@@ -3,6 +3,7 @@ class AgreementControlPerson < ApplicationRecord
   belongs_to :control_person
 
   def self.populate
+    delete_all # Simplest way to ensure records deleted from Airtable do not persist in local database
     Agreement.find_each do |agreement|
       (agreement.fields["Controllers"] || []).each do |control_person_id|
         control_person = ControlPerson.find_by(record_id: control_person_id)
